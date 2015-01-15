5815ea9b80598bd0726d8c01192073803c621ea0
Angus Leigh <angusleigh@gmail.com>
2015-01-14 22:55:39 -0500

    minor cleanup

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ arxiv_scraper
 This is a modification of the arXiv here:
 http://arxiv.org/help/api/user-manual#python_simple_example
 
-I changed it to query the server to retrive all abstracts (not pdfs!) from articles
-of a given category. 
+I changed it to query the server to retrive all abstracts from articles
+of a given category. This does not retrieve the pdf's of the articles 
+(arXiv does not like that http://arxiv.org/help/robots).
 
 I used it to create the dataset for our machine learning class's Kaggle competition: 
 http://inclass.kaggle.com/c/abstract-classification
@@ -15,3 +16,13 @@ Example: arXiv.py stat 5000
 
 This is free software.  Feel free to do what you want
 with it, but please play nice with the arXiv API!
+
+Usage:
+python arXiv.py category max_results
+
+Example: 
+arXiv.py stat 5000
+
+You may have to install the feedparser module
+$ sudo apt-get install python-feedparser
+
--- arXiv.py
+++ arXiv.py
@@ -1,11 +1,15 @@
 """
-arXiv_scraper.py
+arXiv.py
 
-This is a modification of the arXiv here:
-http://arxiv.org/help/api/user-manual#python_simple_example
+This is a modification of the arXiv parser here:
+http://arxiv.org/help/api/examples/python_arXiv_parsing_example.txt
 
 I changed it to query the server to retrive all abstracts from articles
-of a given category. 
+of a given category. This does not retrieve the pdf's of the articles 
+(arXiv does not like that, see: http://arxiv.org/help/robots).
+
+I used it to create the dataset for our machine learning class's Kaggle competition: 
+http://inclass.kaggle.com/c/abstract-classification
 
 Command line usage is: arXiv.py category max_results
 Example: arXiv.py stat 5000
@@ -19,76 +23,68 @@ import time
 import urllib
 import feedparser
 import csv
+import os
 
 # Parse input args
 if len(sys.argv) != 3:
-    print "Usage: arXiv.py category max_results"
-    print "Example: arXiv.py stat 5000"
+    print "Usage: python arXiv.py category max_results"
+    print "Example: python arXiv.py stat 5000"
     print "Exiting"
     exit()
 category = sys.argv[1]
 max_total_results = int(sys.argv[2])
 
 # Open csv file to write abstracts to
-ofile  = open('/home/angus/python_workspace/arXiv_scraper/csv/' + category + '.csv', "wb")
+ofile  = open(os.path.dirname(__file__) + category + '.csv', "wb")
 writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL) 
 writer.writerow(['category', 'abstract'])
 
 # Base api query url
-base_url = 'http://export.arxiv.org/api/query?';
+base_url = 'http://export.arxiv.org/api/query?'
 
 # Search parameters
-search_query = 'cat:' + category + '.*' # search for electron in all fields
+search_query = 'cat:'+category+'.*' 
+
+# Opensearch metadata such as totalResults, startIndex, 
+# and itemsPerPage live in the opensearch namespase.
+# Some entry metadata lives in the arXiv namespace.
+# This is a hack to expose both of these namespaces in
+# feedparser v4.1
+feedparser._FeedParserMixin.namespaces['http://a9.com/-/spec/opensearch/1.1/'] = 'opensearch'
+feedparser._FeedParserMixin.namespaces['http://arxiv.org/schemas/atom'] = 'arxiv'
 
 # Iterating so query is broken down into multiple smaller queries
 # with time delays in between so their server doesn't hang up on us
 results_per_iter = 500
-reached_end = False
-for i in xrange(0, max_total_results, results_per_iter): 
-    if reached_end:
-        break
-
+for i in xrange(0,max_total_results,results_per_iter):    
     # Search parameters
     start = i                     
     max_results = results_per_iter
     query = 'search_query=%s&start=%i&max_results=%i' % (search_query,start,max_results)
 
-    # Opensearch metadata such as totalResults, startIndex, 
-    # and itemsPerPage live in the opensearch namespase.
-    # Some entry metadata lives in the arXiv namespace.
-    # This is a hack to expose both of these namespaces in
-    # feedparser v4.1
-    feedparser._FeedParserMixin.namespaces['http://a9.com/-/spec/opensearch/1.1/'] = 'opensearch'
-    feedparser._FeedParserMixin.namespaces['http://arxiv.org/schemas/atom'] = 'arxiv'
-
-    # Repeat GET requests if there's a <Connection timed out> excpetion
+    # Repeat GET requests if there's a <Connection timed out> or some other kind of excpetion
     excepetion_count = 0
-    while True:
+    successful_response = False
+    while successful_response == False:
         try:
             # Perform a GET request using the base_url and query
             response = urllib.urlopen(base_url+query).read()
-            break
+            successful_response = True
         except IOError:
-            print 'IOError exception. Trying again.'
+            print 'IOError exception. Likely a connection time out. Trying again in 20s.'
             time.sleep(20)
             excepetion_count += 1
             if excepetion_count > 10:
-                break
-    if excepetion_count > 10:
-        print 'Too many IOError exceptions. They have likely hung up. Stopping' 
-        break
+                print 'Too many IOError exceptions. They have likely hung up for good. Stopping' 
+                exit()
 
     # Parse the response using feedparser
     feed = feedparser.parse(response)
-
-    # Print out feed information
-    print 'Feed title: %s' % feed.feed.title
-    print 'Feed last updated: %s' % feed.feed.updated
-
+    
     # Print opensearch metadata
-    print 'totalResults for this query: %s' % feed.feed.opensearch_totalresults
-    print 'itemsPerPage for this query: %s' % feed.feed.opensearch_itemsperpage
-    print 'startIndex for this query: %s'   % feed.feed.opensearch_startindex
+    if i == 0:
+    	    print 'Total results for this query: %s' % feed.feed.opensearch_totalresults
+    print 'Recieved: %d results so far' % (i+results_per_iter)
 
     # Run through each entry, and save the abstract and category to a csv file
     for entry in feed.entries:
@@ -96,12 +92,15 @@ for i in xrange(0, max_total_results, results_per_iter):
         abstract = ' '.join(raw_abstract.splitlines())
         writer.writerow([category, abstract])
     
-    # Time delay so we don't overload their server, causing them to hang up on us
-    time.sleep(5)
+	print type(feed.feed.opensearch_itemsperpage)
+	print feed.feed.opensearch_itemsperpage
 
-    # Stopping condition
+    # Need additional stopping condition in case the results available < max_total_results
     if feed.feed.opensearch_itemsperpage < max_results:
-        reached_end = true
+        break
+    
+    # Time delay so we don't overload their server, causing them to hang up on us
+    time.sleep(5)    
 
-print 'Finished successfully!'
+print 'Finished!'
 ofile.close()
